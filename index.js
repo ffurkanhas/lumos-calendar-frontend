53308cafe8c43b90abe1d94e59836c482dc76052
Emre Kaan Suslu <emrekaansuslu@gmail.com>
2018-07-08 20:42:08 +0300

    Layout is modified

--- index.js
+++ index.js
@@ -31,6 +31,7 @@ $.ajax({
 
 function getData() {
 $.get( "https://lumos-calendar-api-deneme.herokuapp.com/api/v1/calendars", function( data ) {
+  console.log(data);
 return data;
   });
 }
@@ -48,8 +49,8 @@ function send() {
   var title = document.getElementById("title").value;
   var description = document.getElementById("description").value;
   var date = document.getElementById("date").value;
-  var start_time = document.getElementById("start_time").value;
-  var end_time = document.getElementById("end_time").value;
+  var start_time = document.getElementById("start_time_hour").value+":"+document.getElementById("start_time_min").value;
+  var end_time = document.getElementById("end_time_hour").value+":"+document.getElementById("end_time_min").value;
   var reminder = document.getElementById("reminder").value;
   var recurring = document.getElementById("recurring").value;
 
@@ -74,5 +75,44 @@ $.ajax({
     }
 
 });
+
+}
+
+function tableCreate() {
+    var body = document.getElementById("appointments");
+    var tbl = document.createElement('table');
+    tbl.setAttribute('class', 'table');
+    var thead = document.createElement('thead');
+    tbl.style.width = '100%';
+    tbl.setAttribute('border', '0');
+    var tbdy = document.createElement('tbody');
+
+    var date = document.getElementById("date").value;
+    var dates = search("deneme");
+    console.log(dates)
+
+    var tr = document.createElement('tr');
+    var th1 = document.createElement('th');
+    var th1_text = document.createTextNode("Title");
+    th1.appendChild(th1_text);
+    tr.appendChild(th1);
+
+      var th2 = document.createElement('th');
+    var th2_text = document.createTextNode("Descripton");
+    th2.appendChild(th2_text);
+    tr.appendChild(th2);
+      var th3 = document.createElement('th');
+    var th3_text = document.createTextNode("Start Time");
+    th3.appendChild(th3_text);
+    tr.appendChild(th3);
+      var th4 = document.createElement('th');
+    var th4_text = document.createTextNode("End Time");
+    th4.appendChild(th4_text);
+    tr.appendChild(th4);
+    thead.appendChild(tr);
+    tbl.appendChild(thead);
+    tbl.appendChild(tbdy);
+     body.appendChild(tbl);
+
 
 }
