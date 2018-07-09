dfd52f46ba3a275faf44bbc05acf3b0e9c1ff04f
Emre Kaan SÜSLÜ <emrekaansuslu@gmail.com>
2018-07-09 13:28:28 +0300

    fix

--- index.js
+++ index.js
@@ -1,51 +1,19 @@
-
-
-function sendData() {
-
-  var title = document.getElementById("title").value;
-  var description = document.getElementById("description").value;
-  var start_date = document.getElementById("s_date").value;
-  var end_date = document.getElementById("e_date").value;
-  var reminder = document.getElementById("reminder").value;
-  var myObject = {"userid":"null",
-                  "title":title,
-                  "description":description,
-                  "start_date":start_date,
-                  "end_date":end_date,
-                  "reminder":reminder,
-                  "recurring":"null"};
-
-$.ajax({
-
-    url:'https://lumos-calendar-api-deneme.herokuapp.com/api/v1/calendars',
-    type:'POST',
-    dataType:'json',
-    data : JSON.stringify(myObject),
-     contentType: "application/json; charset=utf-8",
-    success:function(data) {
-      console.log(data.title);
-    }
-
-});
-}
+var baseUrl = "https://lumos-calendar-api.herokuapp.com/calendar/api/v1/events";
 
 function getData() {
-$.get( "https://lumos-calendar-api-deneme.herokuapp.com/api/v1/calendars", function( data ) {
+$.get(baseUrl, function( data ) {
   console.log(data);
-return data;
+  return data;
   });
 }
 
 function search(key) {
-
-  $.get( "https://lumos-calendar-api-deneme.herokuapp.com/api/v1/calendars/search/"+key, function( data ) {
-return data;
+  $.get(baseUrl + "/search/" + key, function( data ) {
+  return data;
   });
-
 }
 
 function send() {
-
   var title = document.getElementById("title").value;
   var description = document.getElementById("description").value;
   var date = document.getElementById("date").value;
@@ -54,32 +22,31 @@ function send() {
   var reminder = document.getElementById("reminder").value;
   var recurring = document.getElementById("recurring").value;
 
-    var myObject = {"userid":"null",
-                  "title":title,
-                  "description":description,
-                  "date":date,
-                  "start_time":start_time,
-                  "end_time":end_time,
-                  "reminder":reminder,
-                  "recurring":"null"};
+  var myObject = {"user_id":"1",
+                "title":title,
+                "description":description,
+                "start_time": date + "T" + start_time,
+                "end_time": date + "T" + end_time,
+                "reminder":reminder,
+                "recurring":"null"};
 
-$.ajax({
+  $.ajax({
 
-    url:'https://lumos-calendar-api-deneme.herokuapp.com/api/v1/calendars',
-    type:'POST',
-    dataType:'json',
-    data : JSON.stringify(myObject),
-     contentType: "application/json; charset=utf-8",
-    success:function(data) {
-      
-    }
-
-});
+      url:baseUrl,
+      type:'POST',
+      dataType:'json',
+      data : JSON.stringify(myObject),
+      contentType: "application/json; charset=utf-8",
+      success:function(data) {
+        
+      }
+  });
 
 }
 
 function tableCreate() {
     var body = document.getElementById("appointments");
+    body.innerHTML = "";
     var tbl = document.createElement('table');
     tbl.setAttribute('class', 'table');
     var thead = document.createElement('thead');
@@ -88,7 +55,7 @@ function tableCreate() {
     var tbdy = document.createElement('tbody');
 
     var date = document.getElementById("date").value;
-    var dates = search("deneme");
+    var dates = search("sprint");
     console.log(dates)
 
     var tr = document.createElement('tr');
@@ -97,22 +64,22 @@ function tableCreate() {
     th1.appendChild(th1_text);
     tr.appendChild(th1);
 
-      var th2 = document.createElement('th');
+    var th2 = document.createElement('th');
     var th2_text = document.createTextNode("Descripton");
     th2.appendChild(th2_text);
     tr.appendChild(th2);
-      var th3 = document.createElement('th');
+    var th3 = document.createElement('th');
     var th3_text = document.createTextNode("Start Time");
     th3.appendChild(th3_text);
     tr.appendChild(th3);
-      var th4 = document.createElement('th');
+    var th4 = document.createElement('th');
     var th4_text = document.createTextNode("End Time");
     th4.appendChild(th4_text);
     tr.appendChild(th4);
     thead.appendChild(tr);
     tbl.appendChild(thead);
     tbl.appendChild(tbdy);
-     body.appendChild(tbl);
+    body.appendChild(tbl);
 
 
 }
