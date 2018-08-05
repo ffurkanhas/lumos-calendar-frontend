843f2fdba4c917ea378223a7fd107c153b2d9639
Emre Kaan Suslu <emrekaansuslu@gmail.com>
2018-08-05 20:10:44 +0300

    Delete is add

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ var datas;
 var data_length;
 var lgt_search ;
 var arananDatas;
+var buttons = new Array();
+var ids = new Array();
+var map = new Map();
+
 
 function getData() {
   $.ajax({
@@ -23,12 +27,15 @@ function deleteData(id) {
       type:'DELETE',
       contentType: "application/json; charset=utf-8",
       success:function(data) {
+        console.log(id)
         location.reload();
       }
   });
 }
 
-function search(key) {
+function search(key,callback) {
+
+
   $.ajax({
       url:baseUrl + "/search/" + key,
       type:'GET',
@@ -43,7 +50,7 @@ function search(key) {
         div.innerHTML="";
         var tbl = document.createElement('table');
         tbl.setAttribute('class', 'table table-striped');
-        tbl.setAttribute('id', 'table');
+        tbl.setAttribute('id', 'search_table');
 
         var thead = document.createElement('thead');
         var tr = document.createElement('tr');
@@ -73,11 +80,6 @@ function search(key) {
         th4.appendChild(th4_text);
         tr.appendChild(th4);
 
-        var th5 = document.createElement('th');
-        var th5_text = document.createTextNode("Delete");
-        th5.appendChild(th5_text);
-        tr.appendChild(th5);
-
         thead.appendChild(tr);
         tbl.appendChild(thead);
 
@@ -90,14 +92,13 @@ function search(key) {
         tbl.appendChild(tbody);
         div.appendChild(tbl);
 
+
+
+
         for(var i = 0; i < lgt_search; i++) {
           var id = arananDatas[i].id;
-          var btn = document.createElement('BUTTON');
-          btn.className = "btn btn-danger";
-          btn.onclick = function() {
-            //buraya bi seyler yapman lazim id'yi gormuyo burada anlamadim deleteData() fonksiyonu var yukarida orada ona id'yi gonder
-          };
-          btn.innerHTML = "Delete";
+ 
+
 
           var start_time = arananDatas[i].start_time;
           var start_time_split = start_time.split("T");
@@ -112,17 +113,20 @@ function search(key) {
           var cell2 = row.insertCell(2);
           var cell3 = row.insertCell(3);
           var cell4 = row.insertCell(4);
-          var cell5 = row.insertCell(5);
+
 
           cell0.innerHTML = arananDatas[i].id;
           cell1.innerHTML = arananDatas[i].title;
           cell2.innerHTML = arananDatas[i].description;
           cell3.innerHTML = start_time_split[0] + " " + start_time_split[1];
           cell4.innerHTML = end_time_split[0] + " " + end_time_split[1];
-          cell5.appendChild(btn);
+
         }
+
       }
   });
+
+
 }
 
 function send() {
@@ -139,7 +143,7 @@ function send() {
                   "start_time": date + "T" + start_time,
                   "end_time": date + "T" + end_time,
                   "reminder":reminder,
-                  "recurring":null};
+                  "recurring":recurring};
   $.ajax({
       url:baseUrl,
       type:'POST',
@@ -153,7 +157,7 @@ function send() {
   });
 }
 
-function showAppointment() {
+function showAppointment(callback) {
         var div = document.getElementById("appointments");
 
         div.innerHTML="";
@@ -186,6 +190,10 @@ function showAppointment() {
         tr.appendChild(th4);
         thead.appendChild(tr);
         tbl.appendChild(thead);
+        var th5 = document.createElement('th');
+        var th5_text = document.createTextNode("Delete");
+        th5.appendChild(th5_text);
+        tr.appendChild(th5);
 
         var tbody= document.createElement('tbody');
 
@@ -201,10 +209,20 @@ function showAppointment() {
 
         var date = document.getElementById("date").value;
 
+        var button = new Array();
+        var id_current = new Array();
+
         for(var i =0;i<data_length;i++) {
           var str = datas[i].start_time;
           var str_split = str.split("T");
           var tarih = str_split[0];
+
+          var btn = document.createElement('BUTTON');
+          btn.className = "btn btn-danger";
+          btn.innerHTML = "Delete";
+
+          
+
 
           if(tarih == date) {
             var start_time = datas[i].start_time;
@@ -220,12 +238,45 @@ function showAppointment() {
             var cell2 = row.insertCell(2);
             var cell3 = row.insertCell(3);
             var cell4 = row.insertCell(4);
+            var cell5 = row.insertCell(5);
+            cell5.appendChild(btn);
+            button.push(btn);
 
             cell0.innerHTML = datas[i].id;
             cell1.innerHTML = datas[i].title;
             cell2.innerHTML = datas[i].description;
             cell3.innerHTML = start_time_split[0] + " " + start_time_split[1];
             cell4.innerHTML = end_time_split[0] + " " + end_time_split[1];
+            id_current.push(datas[i].id)
+
+            map.set(datas[i].id,btn)
+           // map[datas[i].id] =btn;
           }
         }
+
+        buttons = button;
+        ids = id_current;
+        callback();
+}
+
+function addListener() {
+
+  console.log("Fonksiyona girdim");
+
+console.log(buttons)
+console.log(ids)
+
+for(var i =0 ; i<buttons.length; i++) {
+
+    my_id = ids[i];
+    btn = map.get(ids[i])
+    btn.addEventListener("click",function(){
+    console.log("EKLIYORUM"+my_id);
+    deleteData(my_id);
+    });
+    console.log("EKLENEN ID"+my_id)
+
+    
+}
+
 }
